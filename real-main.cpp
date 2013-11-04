2d9b348fd095c9038137814ce1b3b87acddbaacb
DepthDeluxe <cjh025@bucknell.edu>
2013-11-04 01:30:04 -0500

    added a simple console echo process (that doesn't crash!) Should be useful for demoing kids

--- real-main.cpp
+++ real-main.cpp
@@ -2,14 +2,51 @@
 #include <wirish/wirish.h>
 
 // setup() and loop():
+static const int BUFFER_SIZE = 256;
+char input;
+char inputBuffer[BUFFER_SIZE];
+int bufferLoc = 0;
 
 void setup(void) {
     pinMode(BOARD_LED_PIN, OUTPUT);
 }
 
 void loop(void) {
-    SerialUSB.println("Hello World!");
-    delay(1000);
+	// echoes whatever is written to the screen
+	SerialUSB.read(&input, 1);
+	SerialUSB.write(&input, 1);
+	//SerialUSB.print((int)input);
+
+	// check for newline
+	if (input == '\r') {
+		SerialUSB.print('\n');
+		SerialUSB.write("-> ", 3);
+
+		bufferLoc = 0;
+	}
+	else if (input == 127) {
+		// blank out the buffer
+		SerialUSB.print('\r');
+		for (int n = 0; n < (bufferLoc + 3); n++) {
+			SerialUSB.write(" ", 1);
+		}
+
+		bufferLoc--;
+		if (bufferLoc < 0) {
+			bufferLoc = 0;
+		}
+
+		SerialUSB.print('\r');
+		SerialUSB.write("-> ", 3);
+		SerialUSB.write(inputBuffer, bufferLoc);
+	}
+	// else, add to the buffer
+	else if (bufferLoc < BUFFER_SIZE) {
+		inputBuffer[bufferLoc++] = input;
+	}
+	else {
+		bufferLoc = 0;
+	}
 }
 
 // Standard libmaple init() and main.
